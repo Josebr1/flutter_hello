019080d1493facb50d876a521f139f4b73315a46
jose.antonio <jose.antonio@7comm.com.br>
2020-03-02 14:59:43 -0300

    Voltando para a tela anterior

--- lib/pages/hello_page2.dart
+++ lib/pages/hello_page2.dart
@@ -2,14 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HelloPage2 extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Page 2'),
       ),
+      body: _body(context),
     );
   }
 
+  _body(context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Voltar'),
+        onPressed: () =>
+          Navigator.pop(context)
+      ),
+    );
+  }
 }
