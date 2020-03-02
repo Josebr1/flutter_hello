01088f3b198038104cbe5f64dded4609bfa728a7
jose.antonio <jose.antonio@7comm.com.br>
2020-03-02 19:15:32 -0300

    Navegação para a tela de detalhes

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,26 +1,112 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-class HelloListView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_hello/pages/dog_page.dart';
+import 'package:flutter_hello/utils/nav.dart';
+
+class Dog {
+  String nome;
+  String foto;
+
+  Dog(this.nome, this.foto);
+}
+
+class HelloListView extends StatefulWidget {
+
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _gridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              print('Lista');
+              setState(() {
+                _gridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              print('Grid');
+              setState(() {
+                _gridView = true;
+              });
+            },
+          ),
+        ],
       ),
       body: _body(),
     );
   }
 
   _body() {
-    return ListView(
-      children: <Widget>[
-        _img("assets/images/dog1.png"),
-        _img("assets/images/dog2.png"),
-        _img("assets/images/dog3.png"),
-        _img("assets/images/dog4.png"),
-        _img("assets/images/dog5.png"),
-      ],
+    List<Dog> dogs = [
+      Dog('Jack Russel', "assets/images/dog1.png"),
+      Dog('Labrador', "assets/images/dog2.png"),
+      Dog('Pug', "assets/images/dog3.png"),
+      Dog('Rottweiller', "assets/images/dog4.png"),
+      Dog('Pastor', "assets/images/dog5.png"),
+    ];
+
+    if(_gridView) {
+      return GridView.builder(
+          gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemCount: dogs.length,
+          itemBuilder: (context, index) {
+            return _itemView(dogs, index);
+          });
+    } else {
+      return ListView.builder(
+          itemCount: dogs.length,
+          itemExtent: 300,
+          itemBuilder: (context, index) {
+            return _itemView(dogs, index);
+          });
+    }
+  }
+
+   _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+
+    return GestureDetector(
+      onTap: () {
+        push(context, DogPage(dog));
+      },
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          _img(dog.foto),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.all(12.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Text(
+                dog.nome,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
